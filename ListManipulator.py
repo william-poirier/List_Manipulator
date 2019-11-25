fc45682ddb8b1d10cec4a3e5c7cff9c9e95b01e0
william-poirier <william.poirier@my.columbiasc.edu>
2019-11-25 23:41:11 +0000

    Added two new methods to ListManipulatorTest - testMin and testRemove Also commented the three definitions Re-added ListManipulator.py after accidentally removing it from the Git Oops

---
+++ ListManipulator.py
@@ -0,0 +1,72 @@
+import unittest
+
+
+class ListManipulator:
+    def __init__(self, list):
+        self.list = list
+
+    def min(self):
+        if len(self.list) == 0:
+            return None
+
+        min = self.list[0]
+        for item in self.list:
+            if item < min:
+                min = item
+        return min
+
+    def max(self):
+        if len(self.list) == 0:
+            return None
+
+        max = self.list[0]
+        for item in self.list:
+            if item > max:
+                max = item
+        return max
+
+    def remove(self, value):
+        to_remove = []
+        for i, item in enumerate(self.list):
+            if item == value:
+                to_remove.append(i)
+
+        removed_count = 0
+        for index in to_remove:
+            self.list.pop(index - removed_count)
+            removed_count += 1
+
+
+class ListManipulatorTest(unittest.TestCase):
+    def testMax(self):
+        # Create a list and check the max of the list (will max() return 4?  It should)
+        _list = [1, 2, 3, 4]
+        manipulator = ListManipulator(_list)
+        self.assertEqual(manipulator.max(), 4)
+
+        # Create another list and manipulator, this time with unordered integers and floats mixed in
+        _list = [2, 1.4, -5, 12.7, -135.9845]
+        manipulator = ListManipulator(_list)
+        self.assertEqual(manipulator.max(), 12.7)
+
+    def testMin(self):
+        # Create a list and check the min of the list (will min() return 1?  It should)
+        _list = [1, 2, 3, 4]
+        manipulator = ListManipulator(_list)
+        self.assertEqual(manipulator.min(), 1)
+
+        # Create a new list of unordered integers and floats
+        _list = [-2, 2.784, 8952.5, 1, -10.1]
+        manipulator = ListManipulator(_list)
+        self.assertEqual(manipulator.min(), -10.1)
+
+    def testRemove(self):
+        # Create a list and check to see if the index was removed correctly
+        _list = [1, 2, 3, 4]
+        manipulator = ListManipulator(_list)
+        manipulator.remove(2)
+        self.assertEqual(2 in manipulator.list, False)
+
+
+# Time to test everything
+unittest.main()
